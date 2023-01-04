b576d8ebc2fbda95741dd79122f249d20adef0a5
bened <uwoiy@student.kit.edu>
2023-01-04 15:53:50 +0100

    init and basic functions

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,69 @@
 import { Component } from '@angular/core';
-import { RefresherCustomEvent } from '@ionic/angular';
 
-import { DataService, Message } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-  constructor(private data: DataService) { }
+  constructor() { }
 
-  refresh(ev: any) {
-    setTimeout(() => {
-      (ev as RefresherCustomEvent).detail.complete();
-    }, 3000);
+  coordinates: any;
+  rome: [number, number] = [41.4536, 12.3209];//
+  direction: [boolean,boolean,boolean,boolean] = [false, false, false, false] //nord, süd, ost, west
+  outputDirection: String = "noch keine Daten bitte warten.";
+  alpha: any;
+  isIOS =
+      navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+      navigator.userAgent.match(/AppleWebKit/);
+
+  trackLocation() {
+
+    if (navigator.geolocation) {
+
+      navigator.geolocation.watchPosition((position) => {
+        this.coordinates = position.coords;
+        this.startCompass();
+        this.calculateDiraction();
+      }, (error) => {alert(error.message)})
+    } else { 
+      this.coordinates = "Geolocation is not supported by this browser.";
+    }
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
+  calculateDiraction(){
+    if(this.coordinates.latitude < this.rome[0]){
+      this.direction[0] = true;
+    }else if(this.coordinates.latitude > this.rome[0]){
+      this.direction[1] = true;
+    }
+
+    if(this.coordinates.longitude < this.rome[1]){
+      this.direction[2] = true;
+    }else if(this.coordinates.longitude > this.rome[1]){
+      this.direction[3] = true;
+    }
+
   }
 
+  startCompass() {
+
+    if(this.isIOS){
+      alert("Ios unterstützt keine stable Magentometer version.")
+    }
+    
+    try {
+            window.addEventListener("deviceorientationabsolute", (event:any) => {
+        this.alpha = event.alpha;
+        alert(`${event.alpha}`);
+        
+      });
+    } catch (error) {
+      
+      alert(error)
+    }
+
+  }
 }
